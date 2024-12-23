e2d029c808276b1e5aac14d52f3b628a62a96ce3
Kabidoye-17 <23365412@studentmail.ul.ie>
2024-12-23 20:08:06 +0000

    sky background with name text

--- my-portfolio-app/src/index.js
+++ my-portfolio-app/src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Background from './Background';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  </React.StrictMode>
+    <Background/>
 );
 
 reportWebVitals();
