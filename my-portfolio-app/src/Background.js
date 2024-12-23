fd8e935ca1fc0e67837a060c42baca41c1b81621
Kabidoye-17 <23365412@studentmail.ul.ie>
2024-12-23 23:18:48 +0000

    nav bar created. toggles between light and dark mode but buttons aren't functional yet

--- my-portfolio-app/src/Background.js
+++ my-portfolio-app/src/Background.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import Sky from './Sky'
+import NavBar from './NavBar';
+import Container from './Container';
+import { useLocalStorage } from '@uidotdev/usehooks';
 
 const BackgroundContainer = styled.div`
  background-size: cover;
@@ -11,10 +14,14 @@ const BackgroundContainer = styled.div`
     display: block; 
 `
 export default function Background() {
+    const [colourScheme, setColourScheme] = useLocalStorage('colourScheme', 'dark')
 
     return (
        <BackgroundContainer>
-            <Sky/>
+        <Container backgroundColor={colourScheme === 'light'? 'rgba(194,233,249,1)' : 'rgb(0, 169, 239, 1)'}>
+        <NavBar setColourScheme={setColourScheme} colourScheme={colourScheme}/>
+            <Sky colourScheme={colourScheme}/>
+    </Container>
         </BackgroundContainer>
     );
 }
--- my-portfolio-app/src/Sky.js
+++ my-portfolio-app/src/Sky.js
@@ -14,17 +14,20 @@ text-shadow:
     0px 1px 4px rgba(12, 12, 13, 0.05); 
     align-self: center;
     padding-left: 30px;
+padding-bottom: 5px
 
 `
 const TextContainer = styled.div`
 align-self: center;
+padding-top: 20px;
+padding-bottom: 20px;
 `
 
-export default function Sky() {
+export default function Sky({colourScheme}) {
 
     return (
 
-    <BackgroundSector colour={"upperSky"}>
+    <BackgroundSector colour={"upperSky"} colourScheme={colourScheme}>
 <TextContainer>
         <Heading>Kelly</Heading>
         <Heading>Abidoye</Heading>
